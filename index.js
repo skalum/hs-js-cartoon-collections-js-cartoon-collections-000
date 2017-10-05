709045e9357bf93395c09d3a7b9a4b0e70001c50
skalum Kalum <stkalum@gmail.com>
2017-10-05 16:29:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function dwarfRollCall(dwarves) {
   const numDwarves = dwarves.length;
-  var numberedDwarves = [];
+  var dwarfString = "";
 
   for (let i = 0; i < numDwarves; i++) {
-    numberedDwarves.push(`${i + 1}. ${dwarves[i]}`)
+    dwarfString += `${i + 1}. ${dwarves[i]} `;
   }
 
-  return numberedDwarves.join(" ");
+  return dwarfString;
 }
 
 function summonCaptainPlanet(planeteerCalls){
