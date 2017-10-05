2479d55daab5eaedfeeb6607862e1649a3ef7570
skalum Kalum <stkalum@gmail.com>
2017-10-05 16:36:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,29 @@ function dwarfRollCall(dwarves) {
   }
 
   return dwarfString;
-}
+} 
+
+/* Here's how I would do it without the space at the end:
+function dwarfRollCall(dwarves) {
+  const numDwarves = dwarves.length;
+  var numberedDwarves = [];
+
+  for (let i = 0; i < numDwarves; i++) {
+    numberedDwarves.push(`${i + 1}. ${dwarves[i]}`)
+  }
+
+  return numberedDwarves.join(" ");
+} */
 
 function summonCaptainPlanet(planeteerCalls){
+  const numPlaneteers = planeteerCalls.length;
+  var allCapsCalls = [];
+
+  for (let i = 0; i < numPlaneteers; i++) {
+    allCapsCalls.push(`${planeteerCalls[i].toUpperCase}!`);
+  }
+
+  return allCapsCalls;
 }
 
 function longPlaneteerCalls(words) {
